881c80208fae01c90799bee842cf8721ce9b61c7
whwo2142@naver.com <whwo2142@naver.com>
2020-04-20 13:23:17 +0900

    3-5-5최종

--- ch02/ch-3-3-5.cpp
+++ ch02/ch-3-3-5.cpp
@@ -6,6 +6,32 @@ typedef struct {
 	int value;
 } term;
 
+void smTranspose(term a[], term b[]);
+
+int main()
+{
+	term a[11] = {
+		{8,7,10},
+		{0,2,2},
+		{0,6,12},
+		{1,4,7},
+		{2,0,23},
+		{3,3,31},
+		{4,1,14},
+		{4,5,25},
+		{5,6,6},
+		{6,0,52},
+		{7,4,11}
+	};
+	term b[11] = { 0, };
+
+	smTranspose(a, b);
+
+	for (int i = 0; i < b[0].value + 1; i++)
+	{
+		printf("%d %d %d \n", b[i].row, b[i].col, b[i].value);
+	}
+}
 void smTranspose(term a[], term b[]) {
 	int m, n, v, i, j, p;
 	m = a[0].row;
