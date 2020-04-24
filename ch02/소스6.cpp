07c1493a4c6f59eb0100d1757136dd6bc0190a87
whwo2142@naver.com <whwo2142@naver.com>
2020-04-24 11:57:40 +0900

    예제 4-1

---
+++ ch02/소스6.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#define CRT_SECURE_NO_WARNING
+
+int fibo(int num);
+
+int main(void) {
+
+    int num;
+
+    int i;
+
+    printf("\n피보나치 수 입력 : \n");
+
+    scanf_s("%d", &num);
+
+    for (i = 0; i < num; i++) {
+
+        printf("%d ", fibo(i));
+    }
+    printf("\n\n");
+
+    return 0;
+
+}
+
+
+
+int fibo(int num) {
+
+    if (num == 0) return 0;
+
+    else if (num == 1) return 1;
+
+    else return fibo(num - 1) + fibo(num - 2);
+
+}
