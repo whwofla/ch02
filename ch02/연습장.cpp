438fb648622debd3f26b1c186102eab5656f2a75
whwo2142@naver.com <whwo2142@naver.com>
2020-06-01 14:38:46 +0900

    1

--- ch02/연습장.cpp
+++ ch02/연습장.cpp
@@ -1,49 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-typedef int element;
+int main(void) {
+    int a;
+    int b;
+   
+    scanf_s("%d %d", &a, &b);
+    for (int i= 0; i < b; i++) {
+        printf("\n");
+        for (int j = 0; j < a; j++) {
+            printf("*");
+        }
+        printf("%d", a + b);
+    }
 
-typedef struct stackNode {
-
-	element data;
-	struct stackNode* link;
-}stackNode;
-
-stackNode* top;
-
-element isEmpty() {
-	if (top == NULL) {
-		printf("이건 비어있습니다");
-		return 1;
-	}
-	else
-		return 0;
-}
-
-//스택의 top에 원소를 삽입하는 연산
-void push(element item) {
-	stackNode* temp = (stackNode*)malloc(sizeof(stackNode));
-
-	temp->data = item;
-	temp->link = top;
-	top = temp;
-}
-
-//스택의 top에서 원소를 삭제하는 연산
-
-element pop(){
-	element item;
-	stackNode* temp = top;
-
-	if (top == NULL) {
-		printf("비어있습니다");
-		return 0;
-	}
-	else {
-		item = temp->data;
-		top = temp->link;
-		free(temp);
-		return item;
-	}
+    return 0;
 }
